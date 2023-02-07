4ce66d7e944bf484b2307b2b7175e9be4c4e1ab2
Paulowollf <Paulo_hml1@hotmail.com>
2023-02-06 22:29:57 -0300

    Site

---
+++ scrollreveal.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal({ reset: true})
+ScrollReveal().reveal( '.area-1', {
+    delay: 200,
+    
+}) 
+ScrollReveal().reveal( '.area-2', {
+    delay: 100,
+    rotate:{
+        x:10,
+        y:0,
+        z:0
+
+    }
+}) 
+ScrollReveal().reveal( '.area-3', {
+    delay: 400,
+    rotate:{
+        x:0,
+        y:0,
+        z:0
+
+    }
+}) 
+
+
